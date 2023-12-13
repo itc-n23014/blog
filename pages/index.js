0b048d34950d4f3060c254cbb728af498d556ed5
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2023-12-13 12:33:51 +0000

    p56,57 finished
    カスタムAppコンポーネントでLayoutを全ページに入れる

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-import Layout from 'components/layout'
 import Hero from 'components/hero'
 
-const Home = () => {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  )
-}
+const Home = () => <Hero />
 
 export default Home
