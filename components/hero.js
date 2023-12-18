9b90790d820f571406121d981dbb0be0b16d7d89
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2023-12-18 11:04:24 +0000

    p90 finished
    HeroコンポーネントにCSSを適用（2)

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,11 @@ import styles from 'styles/hero.module.css'
 const Hero = props => {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
-      {props.imageOn && <figure> [画像] </figure>}
+      <div className={styles.text}>
+        <h1 className={styles.title}>{props.title}</h1>
+        <p className={styles.subtitle}>{props.subtitle}</p>
+        {props.imageOn && <figure> [画像] </figure>}
+      </div>
     </div>
   )
 }
