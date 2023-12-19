9ab2989d49b079beaf134a06343ea346dcbcc1ea
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2023-12-19 06:43:19 +0000

    editted hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,12 +1,10 @@
 import styles from 'styles/hero.module.css'
 const Hero = props => {
   return (
-    <div>
-      <div className={styles.text}>
-        <h1 className={styles.title}>{props.title}</h1>
-        <p className={styles.subtitle}>{props.subtitle}</p>
-        {props.imageOn && <figure> [画像] </figure>}
-      </div>
+    <div className={styles.text}>
+      <h1 className={styles.title}>{props.title}</h1>
+      <p className={styles.subtitle}>{props.subtitle}</p>
+      {props.imageOn && <figure> [画像] </figure>}
     </div>
   )
 }
