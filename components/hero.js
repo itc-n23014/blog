a09ddcce046a3fa2a7123bd04c05e20bc24c8a00
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2023-12-21 06:57:19 +0000

    error editted

--- components/hero.js
+++ components/hero.js
@@ -1,20 +1,13 @@
 import styles from 'styles/hero.module.css'
-<<<<<<< HEAD
-=======
 import Image from 'next/image'
 import cube from 'images/cube.jpg'
 
->>>>>>> chapter5
 const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
-    <div className={styles.flexCotainer}>
+    <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
-<<<<<<< HEAD
-        {imageOn && <figure> [image] </figure>}
-=======
->>>>>>> chapter5
       </div>
       {imageOn && (
         <figure>
