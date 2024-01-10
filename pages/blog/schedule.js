b700b10114e2d9516f25652ccb2f890fe96e5b62
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-10 07:50:01 +0000

    p234-239 finished
    記事の本文を表示する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,10 +1,41 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import Image from 'next/image'
+import ConvertBody from 'components/convert-body'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
-      <h1>記事のタイトル</h1>
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
