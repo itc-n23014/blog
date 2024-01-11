93c2a6b74532c67ef5fcdeb3918c3be5d243ba4b
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-11 05:42:20 +0000

    p248-251 finished
    アイキャッチのブラー適用

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,8 @@ import PostCategories from 'components/post-categories'
 import ConvertBody from 'components/convert-body'
 import { extractText } from 'lib/extract-text'
 import Meta from 'components/meta'
+import { eyecatchLocal } from 'lib/constants'
+import { getPlaiceholder } from 'plaiceholder'
 
 const Schedule = ({
   title,
@@ -41,6 +43,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -61,16 +65,19 @@ const Schedule = ({
 export default Schedule
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description
     }
