fe241d02d21ffab5ec011b5b24f133e44108e7a2
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-16 06:01:02 +0000

    p272-276
    記事一覧にアイキャッチ画像を追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,8 @@ import Container from 'components/container'
 import Meta from 'components/meta'
 import { getAllPosts } from 'lib/api'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -16,8 +18,16 @@ const Blog = ({ posts }) => {
 
 export default Blog
 
-export async function getStatic () {
+export async function getStaticProps () {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
