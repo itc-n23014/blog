715bd296203504b0b0320bfb22d10eb807ef41db
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-16 06:52:44 +0000

    chapter9　最終編集

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
+import { getAllPosts } from 'lib/api'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
 import Meta from 'components/meta'
+import { eyecatchLocal } from 'lib/constants'
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
-    <>
-      <Container>
-        <Meta />
-        <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
-      </Container>
-    </>
+    <Container>
+      <Meta />
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
+    </Container>
   )
 }
-
 export default Home
+
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
